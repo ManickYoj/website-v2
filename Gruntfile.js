2338c6835f602699e5d166e29fa432dbc98b72b8
Nick Francisci <nickfrancisci@gmail.com>
2016-11-04 03:34:43 -0400

    Ported over much of existing site content. Index not fully reformatted in site generator.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,26 @@
 module.exports = function(grunt) {
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		assemble: {
+			options: {
+				partials: ["src/partials/*"],
+				assets: "src/assets",
+				layout: "src/layouts/default.hbs",
+				flatten: "true"
+			},
+			pages: {
+				files: {
+					'web/': ['src/pages/*.hbs']
+				}
+			}
+		},
+		//Delete cached HTML files before recompiling
+		clean: {
+			all: ['web/*.html']
+		}
 	});
-	grunt.loadNpmTasks('assemble');
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-assemble');
+	grunt.registerTask('default', ['clean', 'assemble']);
 }
